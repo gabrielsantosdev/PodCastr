ee2acacaec4bd6b9759a693837f06fcfd31364c8
Gabriel <moreira.gabriel367@gmail.com>
2021-04-22 12:03:57 -0300

    Initial NextJS with SCSS and Typescript utilizing NLW (Rocketseat) Project

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+// SPA
+// SSR
+// SSG
+
+import { useEffect } from "react";
+
+export default function Home(props) {
+  console.log(props.episodes);
+
+  return <h1>Main Page</h1>;
+}
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
+}
